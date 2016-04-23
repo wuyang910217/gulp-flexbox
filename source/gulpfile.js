a280bd03a021fd800b898f0cbc04af2c10356bf2
wuyang910217 <wuyang910217@gmail.com>
2016-04-23 18:43:08 +0800

    add gulp-wrap and browser-sync

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -1,6 +1,26 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
+var wrap = require('gulp-wrap');
+var browserSync = require('browser-sync');
+
+gulp.task('browser-sync',['sass', 'build'], function(){
+  browserSync({
+    server: {
+      baseDir: '..'
+    }
+  });
+});
+
+gulp.task('build', function(){
+  gulp.src("pages/*.html")
+          .pipe(wrap({src:"layout/default.html"}))
+          .pipe(gulp.dest('..'));
+});
+
+gulp.task('rebuild',['build'], function(){
+  browserSync.reload();
+});
 
 function handleError(err) {
   console.log(err.toString());
@@ -10,17 +30,13 @@ gulp.task('sass',function(){
   gulp.src('styles/main.scss')
           .pipe(sass()).on('error', handleError)
           .pipe(prefix())
-          .pipe(gulp.dest('../styles'));
-});
-
-gulp.task('cp', function(){
-  gulp.src('index.html')
-          .pipe(gulp.dest('..'));
+          .pipe(gulp.dest('../styles'))
+          .pipe(browserSync.reload({stream:true}));
 });
 
 gulp.task('watch', function(){
-  gulp.watch(['*.html'], ['cp']);
+  gulp.watch(['**/*.html'], ['rebuild']);
   gulp.watch(['styles/*.scss'], ['sass']);
 });
 
-gulp.task('default',['sass', 'cp', 'watch']);
+gulp.task('default',['browser-sync', 'watch']);
